1dea745b886f276842f62cd8f7abe8073e7f69c6
Apoorv <apoorvarnav1@gmail.com>
2026-01-10 20:59:40 +0530

    chore: err fix for prev commit

--- src/app/(student-auth)/practice-sets/layout.tsx
+++ src/app/(student-auth)/practice-sets/layout.tsx
@@ -4,7 +4,7 @@ import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 import { setReviewMode } from "@/lib/mock-tests/indexedDb";
 import { useEffect } from "react";
 
-export default async function DemoLayout({ children }: { children: React.ReactNode }) {
+export default function DemoLayout({ children }: { children: React.ReactNode }) {
 
     // turn OFF isReviewMode on load
     // hack to production-only race condition between AnswerInput and isReviewMode
