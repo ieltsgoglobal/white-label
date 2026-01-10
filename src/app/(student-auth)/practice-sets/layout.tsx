d4856cb52650f4587dc6c545116c21af294cc524
Apoorv <apoorvarnav1@gmail.com>
2026-01-10 20:52:50 +0530

    fix: add hack to prevent AnswerInput.tsx reading isReviewMode faster than its being updated on render

--- src/app/(student-auth)/practice-sets/layout.tsx
+++ src/app/(student-auth)/practice-sets/layout.tsx
@@ -1,5 +1,16 @@
+"use client"
+
 import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
+import { setReviewMode } from "@/lib/mock-tests/indexedDb";
+import { useEffect } from "react";
 
 export default async function DemoLayout({ children }: { children: React.ReactNode }) {
+
+    // turn OFF isReviewMode on load
+    // hack to production-only race condition between AnswerInput and isReviewMode
+    useEffect(() => {
+        setReviewMode(false)
+    }, [])
+
     return <AdminPanelLayout>{children}</AdminPanelLayout>;
 }
